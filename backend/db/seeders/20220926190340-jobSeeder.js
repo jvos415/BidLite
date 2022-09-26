01e9c6c81e591212a6f9634bdd23ea61efb0ea42
Jvos <john.voskuyl@gmail.com>
2022-09-26 21:48:58 +0200

    tyler original seeds entered

--- backend/db/seeders/20220926190340-jobSeeder.js
+++ backend/db/seeders/20220926190340-jobSeeder.js
@@ -59,6 +59,78 @@ module.exports = {
           fixtures: 9,
           cost: 25058.67
         },
+        {
+          userId: 2,
+          jobTitle: "Julie Haskell - 33 Edwards",
+          description: "Luxury master bathroom remodel with all new piping and locations.",
+          fixtureList: "shower, built-in tub, dual vanities, toilet. Relocated faucets from upstairs to downstairs. Change order to move shower valve.",
+          fixtures: 6,
+          cost: 17768.61
+        },
+        {
+          userId: 2,
+          jobTitle: "845 Fifth Avenue - Units 17 and 25 Remodel",
+          description: "Re-pipe two units, one above the other, with new ABS plastic and copper. Did not provide new fixtures.",
+          fixtureList: "2x Tub/Shower, 2x Vanities, 2x Toilets, 2x Kitchen Sinks.",
+          fixtures: 8,
+          cost: 15133.85
+        },
+        {
+          userId: 2,
+          jobTitle: "389 County View",
+          description: "Bathroom & laundry room remodel, including rerouting vent pipes and replacing long run of kitchen sink drain.",
+          fixtureList: "1x Tub/Shower, 1x Toilet, 1x Vanity, 1x Washing Machine, Copper Repiping new bathroom and existing bathroom, long run of kitchen sink drain.",
+          fixtures: 6,
+          cost: 12317.59
+        },
+        {
+          userId: 2,
+          jobTitle: "Anna Schmitz & Jean-Claude West",
+          description: "Copper repiping kitchen sink lines, bathroom lines, and replace tub waste & immediate drain.",
+          fixtureList: "1x Tub/Shower, 1x Vanity, 1x Kitchen sink, 1x Toilet. Going to count this as 3 fixtures for the sake of accuracy.",
+          fixtures: 3,
+          cost: 6182.39
+        },
+        {
+          userId: 2,
+          jobTitle: "Scott Myers - 19 Pixley Avenue",
+          description: "Install new gas line to oven in kitchen, install new Earthquake valve, do gas drawings for permit.",
+          fixtureList: "1x oven.",
+          fixtures: 1,
+          cost: 1602.58
+        },
+        {
+          userId: 2,
+          jobTitle: "Bridget Thomas - 15 Lilac",
+          description: "Replace kitchen sink drain line + water lines, relocate gas line for new oven.",
+          fixtureList: "1x kitchen sink, 1x oven.",
+          fixtures: 2,
+          cost: 2746.98
+        },
+        {
+          userId: 2,
+          jobTitle: "Dan & Amanda McAdams",
+          description: "Master Bathroom remodel - replace all existing water lines at new locations and replace old galvi + cast iron DWV.",
+          fixtureList: "1x vanity, 1x floor mount toilet, 1x shower.",
+          fixtures: 3,
+          cost: 7097.02
+        },
+        {
+          userId: 2,
+          jobTitle: "Michele Dugan",
+          description: "Master bathroom addition, tankless water heater installation, hot return loop in long crawl space, kitchenette addition.",
+          fixtureList: "1x freestanding/soaker tub, 1x shower, 2x vanity (double vanity), 1x tankless water heater, 1x toilet, 1x kitchen sink",
+          fixtures: 7,
+          cost: 32835.74
+        },
+        {
+          userId: 2,
+          jobTitle: "Shiloh & Maha Gordon",
+          description: "Re-pipe entire house, and install new tankless water heater including upsizing gas line.",
+          fixtureList: "2x toilets, 3x showers (1 tub/shower, 2x shower valves in master) 1x double vanity, 1x vanity, 1x kitchen sink, 1x pot filler, 1x tankless WH, 1x gas line, 1x entire hot/cold repiping.",
+          fixtures: 12,
+          cost: 30915.81
+        },
     ], {});
   },
 
