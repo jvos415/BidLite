fdcb518c0f0c0785144da3f6bfd27b16d2eea3d3
Jvos <john.voskuyl@gmail.com>
2022-11-07 18:53:25 -0800

    removed unused useSelector

--- frontend/src/components/Jobs/DeleteJob.js
+++ frontend/src/components/Jobs/DeleteJob.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { deleteJob } from "../../store/jobs";
 
 function DeleteJob({ setShowDeleteJobModal, job }) {
