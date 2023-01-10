c36591cffb7a490f4e6173fee25c17dfc53e032d
Jvos <john.voskuyl@gmail.com>
2023-01-09 20:40:49 -0800

    404 page completed

--- frontend/src/components/404/FourOFour.js
+++ frontend/src/components/404/FourOFour.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import oopsRobot from "../../images/404.webp";
+import fourOFour from "../../images/404_page.png"
 import "./fourOFour.css";
 
 function FourOFour() {
@@ -8,7 +8,7 @@ function FourOFour() {
     <main>
       <div className="page-not-found-container">
         <h1 className="page-not-found-title">Page Not Found!</h1>
-        <img id="four-o-four-img" src={oopsRobot} alt="404 Robot"></img>
+        <img id="four-o-four-img" src={fourOFour} alt="404 Robot"></img>
         <NavLink to="/">
           <h3 className="page-not-found-home-link">Back Home</h3>
         </NavLink>
